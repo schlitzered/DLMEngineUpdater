25cb94e6369047738062fcdd011d28cd906c5b5c
Stephan Schultchen <stephan.schultchen@gmail.com>
2022-01-21 11:17:39 +0100

    add retry in release method

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DlmEngineUpdater',
-    version='0.0.3',
+    version='0.0.4',
     description='DlmEngine, distributed lock implementation on top of MongoDB and Redis',
     long_description="""
 DLMEngine implements a restful interface that can be used to implement distributed locks.
