cf4a5d2914ad3f58bc751cc7487a722036383066
Stephan Schultchen <stephan.schultchen@gmail.com>
2022-09-05 10:08:09 +0200

    release 0.0.9: add pre sleep support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DlmEngineUpdater',
-    version='0.0.8',
+    version='0.0.9',
     description='DlmEngine, distributed lock implementation on top of MongoDB and Redis',
     long_description="""
 DLMEngine implements a restful interface that can be used to implement distributed locks.
