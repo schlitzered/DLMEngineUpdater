5ed465627ca6dff5cda4dfb2216e142d2c9fc95c
Stephan Schultchen <stephan.schultchen@factset.com>
2023-01-31 11:31:44 +0100

    quit dlm_engine_updater if day check did not pass

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DlmEngineUpdater',
-    version='0.0.9',
+    version='0.0.10',
     description='DlmEngine, distributed lock implementation on top of MongoDB and Redis',
     long_description="""
 DLMEngine implements a restful interface that can be used to implement distributed locks.
